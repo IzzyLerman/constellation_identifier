5df553be5ed02012afc25d3e6ef20386a551889f
IzzyLerman <izzylerman14@gmail.com>
2025-01-07 09:12:43 -0800

    Training and hyperparameter tuning

--- train_model.py
+++ train_model.py
@@ -1,15 +1,34 @@
 from ultralytics import YOLO
+from ray import tune
 
-model = YOLO('yolov8n.pt')
+model = YOLO('yolo11m.pt')
 
 def train_model():
 
-    results = model.train(data='datasets/data.yaml', epochs=20)
+    results = model.train(data='datasets/data.yaml', epochs=25)
 
     metrics = model.val()  # no arguments needed, dataset and settings remembered
     metrics.box.map  # map50-95
     metrics.box.map50  # map50
-    metrics.box.map75  # map75
+    metrics.box.map75  # map75'''
+    '''
+    search_space = {
+        "hsv_h" : (0.0, 0.1),
+        "hsv_s" : (0.0, 0.9),
+        "hsv_v" : (0.0, 0.9),
+    }
+    
+    model.tune(
+        data="datasets/data.yaml",
+        epochs=30,
+        iterations=300,
+        optimizer="AdamW",
+        space=search_space,
+        plots=True,
+        save=True,
+        val=True,
+    )
+    '''
 
 if __name__ == '__main__':
     train_model()
